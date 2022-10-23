76762a33da5dcbf94c573e71ee82168c66682364
Matias Pompilio <matias.pompilio@nan-labs.com>
2022-10-23 19:32:55 -0300

    feature: useLocalStorage hook

---
+++ apps/playground/src/stories/hooks/useLocalStorage.stories.tsx
@@ -0,0 +1,70 @@
+import React, { FC, PropsWithChildren } from "react";
+import { ComponentMeta } from "@storybook/react";
+import { useLocalStorage } from '@nancy/react-hooks';
+
+const SimpleRowExample: FC<PropsWithChildren> = (props) => {
+  const style = {
+    display: "flex",
+    padding: "20px",
+    border: "2px solid gray",
+    justifyContent: "space-around",
+  };
+
+  return <div style={style}>{props.children}</div>;
+};
+
+const INITIAL_VALUE_ONE = 'initial value';
+const INITIAL_VALUE_TWO = 0;
+const INITIAL_VALUE_THREE = { name: 'John', age: 23 };
+
+
+export const Example: FC = () => {
+  const [valueOne, setValueOne] = useLocalStorage('key-one', INITIAL_VALUE_ONE);
+  const [valueTwo, setValueTwo] = useLocalStorage('key-two', INITIAL_VALUE_TWO);
+  const [valueThree, setValueThree] = useLocalStorage("key-three", INITIAL_VALUE_THREE);
+
+  return (
+    <div>
+      <SimpleRowExample>
+        <div>
+          <code>valueOne:</code>
+          <pre>{valueOne}</pre>
+        </div>
+        <div>
+          <code>valueTwo:</code>
+          <pre>{valueTwo}</pre>
+        </div>
+        <div>
+          <code>valueThree:</code>
+          <pre>Name: {valueThree.name}</pre>
+          <pre>Name: {valueThree.age}</pre>
+        </div>
+      </SimpleRowExample>
+      <SimpleRowExample>
+        <button onClick={() => setValueOne && setValueOne('new value')}>change value one</button>
+        <button onClick={() => setValueTwo && setValueTwo((prev) => prev + 1)}>
+          change value two
+        </button>
+        <button onClick={() => setValueThree && setValueThree((prev) => ({
+          ...prev,
+          name: "Older John",
+          age: prev.age + 1,
+        }))}>
+          change valueThree
+        </button>
+      </SimpleRowExample>
+      <SimpleRowExample>
+      <button onClick={() => setValueOne && setValueOne(INITIAL_VALUE_ONE)}>reset value one</button>
+        <button onClick={() => setValueTwo && setValueTwo(INITIAL_VALUE_TWO)}>
+          reset value two
+        </button>
+        <button onClick={() => setValueThree && setValueThree(INITIAL_VALUE_THREE)}>reset value three</button>
+      </SimpleRowExample>
+    </div>
+  );
+};
+
+export default {
+  title: "Hooks/useLocalStorage",
+  component: Example,
+} as ComponentMeta<typeof Example>;
---
+++ packages/react-hooks/use-local-storage/useLocalStorage.ts
@@ -0,0 +1,48 @@
+import { Dispatch, SetStateAction, useCallback, useState } from "react";
+
+const storeValue = <T>(key: string, value: T): void => {
+  if (!window) return;
+
+  const valueSerialized = JSON.stringify(value);
+  localStorage.setItem(key, valueSerialized);
+};
+
+const getInitialStoredValue = <T>(key: string, initialValue: T): T => {
+  if (!window) return initialValue;
+
+  const storedValue = localStorage.getItem(key);
+  if (storedValue !== null) return JSON.parse(storedValue);
+  else {
+    storeValue(key, initialValue);
+    return initialValue;
+  }
+};
+
+/**
+ * Persist the state with local storage so that it remains after a page refresh. This can be useful
+ * for a dark theme or to record session information. This hook is used in the same way as useState
+ * except that you must pass the storage key in the 1st parameter. If the window object is not
+ * present (as in SSR), useLocalStorage() will return the default value.
+ *
+ * @param key - Key to use in local storage to store value
+ */
+const useLocalStorage = <T>(key: string, initialValue: T): [T, Dispatch<SetStateAction<T>>] | [T] => {
+  const [state, setState] = useState(getInitialStoredValue(key, initialValue));
+
+  const storeState: Dispatch<SetStateAction<T>> = useCallback(
+    (value) => {
+      let newValue = value;
+      if (typeof value === "function") {
+        newValue = (value as ((prevState: T) => T))(state);
+      }
+      setState(newValue);
+      storeValue(key, newValue);
+    },
+    [key, state]
+  );
+
+  if (!window) return [initialValue as T];
+  return [state, storeState];
+};
+
+export default useLocalStorage;
