c6afe7ef63446c7d5f262eb407e5eee0eb411ad3
ulises-jeremias <ulisescf.24@gmail.com>
2022-11-09 03:45:33 -0300

    Fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,5 @@
 module.exports = {
   root: true,
-  // This tells ESLint to load the config from the package `eslint-config-nancy`
-  // and `eslint-config-nancy-ts`.
   extends: ["@nanlabs/eslint-config-ts"],
   parserOptions: {
     sourceType: "module",
