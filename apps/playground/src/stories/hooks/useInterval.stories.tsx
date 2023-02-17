337305f252b0ddb0efb2af622756dd7ae2d26b7e
pauchena <chenapau@gmail.com>
2023-02-17 10:13:45 -0300

    Feature: UseInterval hook

---
+++ apps/playground/src/stories/hooks/useInterval.stories.tsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import { useInterval } from '@nanlabs/react-hooks';
+
+export const Example = () => {
+  const [count, setCount] = useState(0);
+
+  useInterval(() => {
+    setCount(count + 1);
+  }, 1000);
+
+  return <div>{count}</div>;
+};
+
+export default {
+  title: 'Hooks/useInterval',
+  component: useInterval,
+  argTypes: {
+    callback: { control: 'function' },
+    delay: { control: 'number' },
+  },
+};
